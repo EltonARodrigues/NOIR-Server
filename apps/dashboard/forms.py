102baff4da26e72e7205484bc31309e3b17bd309
ThiagoLopes <thiagolopes@protonmail.com>
2018-11-24 18:33:24 -0200

    move code to api app

--- apps/dashboard/forms.py
+++ apps/dashboard/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from .models import Sensor
+from apps.dashboard.models import Sensor
 
 
 class MeasureForm(forms.ModelForm):
--- noir_server/urls.py
+++ noir_server/urls.py
@@ -5,6 +5,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', include('apps.dashboard.urls')),
+    path('api/', include('apps.api.urls')),
 ]
