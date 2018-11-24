f2858dc30de3439ac6cd8dd524679c13e50bd1c5
ThiagoLopes <thiagolopes@protonmail.com>
2018-11-24 14:46:38 -0200

    update models with uuid and another things

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,24 +1,28 @@
-from django.db import models
-from django.db.models import Model
+import uuid
+
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Model, UUIDField
 
 
-class Profile(Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email_confirmed = models.BooleanField(default=False)
+class UUIDModel(Model):
+    class Meta:
+        abstract = True
 
-class Cadastro(Model):
-    title = models.CharField(max_length=50)
+    id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+
+class Sensor(UUIDModel):
+    title = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
-class Valores(Model):
-    idm = models.IntegerField(primary_key=True)
-    data = models.DateField(null=True)
-    temperature = models.FloatField()
-    humidity = models.FloatField()
-    co = models.FloatField()
-    co2 = models.FloatField()
-    mp25 = models.FloatField()
-    id = models.ForeignKey(Cadastro, on_delete=models.CASCADE)
+class GasesCollected(UUIDModel):
+    date = models.DateField(null=True)
+    temperature = models.FloatField(default=0)
+    humidity = models.FloatField(default=0)
+    co = models.FloatField(default=0)
+    co2 = models.FloatField(default=0)
+    mp25 = models.FloatField(default=0)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
