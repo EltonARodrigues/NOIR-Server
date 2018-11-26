3f6729e66a4dbcaeb2f75b905515013f5a6ce2d1
Thiago Lopes <thiagolopes@protonmail.com>
2018-11-26 21:22:04 -0200

    add sensor endpoint

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
 
-from .viewsets import GasesCollectedViewSet
+from .viewsets import GasesCollectedViewSet, SensorViewSet
 
 router = routers.SimpleRouter()
 router.register(r'gases', GasesCollectedViewSet)
+router.register(r'sensors', SensorViewSet)
 
 urlpatterns = router.urls
